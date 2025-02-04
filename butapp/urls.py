abe321fa9f6d4c061f463134b4a8036f10c10da2
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-02-04 04:19:09 +0330

    added gender field to request khabgah site and terms and conditions created

--- butapp/urls.py
+++ butapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register_'),
     path('follow/', views.follow , name='follow'),
+    path('terms_and_conditions/', views.terms_and_conditions , name='terms_and_conditions'),
     path('detail/<int:but_id>/', views.detail, name='detail')
 ]
